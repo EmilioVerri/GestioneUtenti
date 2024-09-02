91fa0ed0f038e73a9b84b826af5aa71f736d8164
EmilioVerri <emilioverri83@gmail.com>
2024-09-02 09:53:05 +0200

    modifiche al filtro e aggiunto il reparto accanto all'utente

--- calendarioMensile.php
+++ calendarioMensile.php
@@ -63,39 +63,38 @@ if (!isset($_SESSION['id'])) {
             window.print();
 
             // Ripristina il contenuto originale
-            document.body.innerHTML = '';
-            document.body.appendChild(calendario);
+            location.reload();
         }
     </script>
 
 
     <!-- Navbar -->
     <nav class="uk-navbar-container uk-margin" uk-navbar>
-            <div class="uk-navbar-left">
-                <ul class="uk-navbar-nav">
-                    <li><a href="gestioneUtenti.php" style="color:black">Home</a></li>
-                    <?php if ($permessi == "admin"): ?>
-                        <li><a href="GestioneUtentiDB.php" style="color:black">Gestione Utenti_DB</a></li>
-                        <li><a href="logAzioniUtenti.php" style="color:black">Log Azioni Utenti_DB</a></li>
-                    <?php endif; ?>
-                    <?php if ($permessi == "admin" || $permessi =="scrittura"): ?>
+        <div class="uk-navbar-left">
+            <ul class="uk-navbar-nav">
+                <li><a href="gestioneUtenti.php" style="color:black">Home</a></li>
+                <?php if ($permessi == "admin"): ?>
+                    <li><a href="GestioneUtentiDB.php" style="color:black">Gestione Utenti_DB</a></li>
+                    <li><a href="logAzioniUtenti.php" style="color:black">Log Azioni Utenti_DB</a></li>
+                <?php endif; ?>
+                <?php if ($permessi == "admin" || $permessi == "scrittura"): ?>
                     <li><a href="inserimentoRichieste.php" style="color:black">Inserimento Richieste</a></li>
-                    <?php endif; ?>
-                    <li><a href="calendarioMensile.php" style="color:black">Calendario Mensile</a></li>
-                    <li><a href="calendarioRichieste.php" style="color:black">Calendario Richieste</a></li>
-                    <li><a href="logout.php" style="color:black">Logout</a></li>
-                </ul>
-            </div>
-            <div class="uk-navbar-right">
-                <a href=""><img src=".\images\logo.png" alt="Logo" width="100" height="100"></a>
-            </div>
-        </nav>
+                <?php endif; ?>
+                <li><a href="calendarioMensile.php" style="color:black">Calendario Mensile</a></li>
+                <li><a href="calendarioRichieste.php" style="color:black">Calendario Richieste</a></li>
+                <li><a href="logout.php" style="color:black">Logout</a></li>
+            </ul>
+        </div>
+        <div class="uk-navbar-right">
+            <a href=""><img src="./images/logo.png" alt="Logo" width="100" height="100"></a>
+        </div>
+    </nav>
 
     <!-- Sezione Calendario -->
 
     <h2>Calendario Mensile</h2>
 
-    <!-- Filtro per data -->
+    <!-- Filtro per data e reparto -->
     <div class="uk-margin">
         <form method="GET" action="calendarioMensile.php">
             <div class="uk-grid-small uk-child-width-auto uk-grid">
@@ -108,10 +107,22 @@ if (!isset($_SESSION['id'])) {
                     <input class="uk-input" type="date" id="data_fine" name="data_fine" required>
                 </div>
                 <div>
+                    <label for="reparto">Reparto:</label>
+                    <select class="uk-select" id="reparto" name="reparto">
+                        <option value="">Tutti</option>
+                        <?php
+                        $reparti_result = $mysqli->query("SELECT nome FROM reparti");
+                        while ($reparto = $reparti_result->fetch_assoc()) {
+                            echo "<option value=\"" . htmlspecialchars($reparto['nome']) . "\">" . htmlspecialchars($reparto['nome']) . "</option>";
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div>
                     <button class="uk-button uk-button-primary" type="submit">Filtra</button>
                 </div>
                 <div>
-                    <button class="uk-button uk-button-default" onclick="stampaCalendario();">Stampa</button>
+                    <button class="uk-button uk-button-default" type="button" onclick="stampaCalendario();">Stampa</button>
                 </div>
             </div>
         </form>
@@ -149,6 +160,7 @@ if (!isset($_SESSION['id'])) {
                 if (isset($_GET['data_inizio']) && isset($_GET['data_fine'])) {
                     $data_inizio = DateTime::createFromFormat('Y-m-d', $_GET['data_inizio']);
                     $data_fine = DateTime::createFromFormat('Y-m-d', $_GET['data_fine']);
+                    $reparto_filtrato = $_GET['reparto'] ?? '';
 
                     $period = new DatePeriod(
                         $data_inizio,
@@ -161,18 +173,27 @@ if (!isset($_SESSION['id'])) {
                         $giorno_settimana = $giorni_traduzione[$giorno_settimana_inglese];
                         $data_formattata = $date->format('Y-m-d');
 
-                        // Estrazione utenti in ferie per il giorno corrente
-                        $stmt = $mysqli->prepare("SELECT utenti.nome, utenti.reparto FROM richieste 
+                        // Estrazione utenti in ferie per il giorno corrente con filtro reparto
+                        $query = "SELECT utenti.nome, utenti.reparto FROM richieste 
                                 JOIN utenti ON richieste.id_utente = utenti.id 
-                                WHERE richieste.data = ?");
-                        $stmt->bind_param('s', $data_formattata);
+                                WHERE richieste.data = ?";
+                        if ($reparto_filtrato) {
+                            $query .= " AND utenti.reparto = ?";
+                        }
+
+                        $stmt = $mysqli->prepare($query);
+                        if ($reparto_filtrato) {
+                            $stmt->bind_param('ss', $data_formattata, $reparto_filtrato);
+                        } else {
+                            $stmt->bind_param('s', $data_formattata);
+                        }
                         $stmt->execute();
                         $result = $stmt->get_result();
 
                         $utenti_ferie = [];
                         $reparti = [];
                         while ($row = $result->fetch_assoc()) {
-                            $utenti_ferie[] = $row['nome'];
+                            $utenti_ferie[] = $row['nome'] . " (" . $row['reparto'] . ")";
                             $reparti[] = $row['reparto'];
                         }
 
